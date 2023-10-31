c53b11d01208a3e7ed73de028d497fbe56494e66
Sergiu Stancioiu <s.sergiu96@gmail.com>
2023-10-31 19:57:04 +0200

    Worked on functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,66 @@
-const currentdate = new Date();
+function calculateAge(birthDate) {
+  const currentDate = new Date();
+  const birthDateObj = new Date(birthDate);
 
-const dayInput = document.querySelector(".day-input");
-const monthInput = document.querySelector(".month-input");
-const yearInput = document.querySelector(".year-input");
+  const ageInMilliseconds = currentDate - birthDateObj;
+  const ageInSeconds = ageInMilliseconds / 1000;
+  const ageInMinutes = ageInSeconds / 60;
+  const ageInHours = ageInMinutes / 60;
+  const ageInDays = ageInHours / 24;
 
-let dayValue = document.querySelector(".day-value");
+  const years = Math.floor(ageInDays / 365);
+  const remainingDays = ageInDays % 365;
+  const months = Math.floor(remainingDays / 30);
+  const days = Math.floor(remainingDays % 30);
 
-console.log(dayInput);
-console.log(monthInput);
-console.log(yearInput);
+  return { years, months, days };
+}
 
-dayInput.addEventListener("input", (event) => {
-  dayValue.textContent = event.target.value; // Update the content of dayValue
-});
+function validateDate() {
+  const dayInput = document.querySelector(".day-input");
+  const monthInput = document.querySelector(".month-input");
+  const yearInput = document.querySelector(".year-input");
+
+  const showYears = document.querySelector(".year-value");
+  const showMonths = document.querySelector(".month-value");
+  const showDays = document.querySelector(".day-value");
+
+  const dayValue = dayInput.value;
+  const monthValue = monthInput.value;
+  const yearValue = yearInput.value;
+
+  if (isNaN(dayValue) || isNaN(monthValue) || isNaN(yearValue)) {
+    document.getElementById("result").textContent =
+      "Invalid input. Please enter valid numbers for dayValue, monthValue, and year.";
+  } else {
+    if (isValidDate(yearValue, monthValue, dayValue)) {
+      document.getElementById("result").textContent = "Valid date!";
+
+      const birthDate = new Date(`${yearValue}-${monthValue}-${dayValue}`);
+
+      const age = calculateAge(birthDate);
+
+      showYears.textContent = age.years;
+      showMonths.textContent = age.months;
+      showDays.textContent = age.days;
+    } else {
+      document.getElementById("result").textContent =
+        "Invalid date. Please enter a valid date.";
+    }
+  }
+}
+
+function isValidDate(yearValue, monthValue, dayValue) {
+  if (
+    yearValue < 1900 ||
+    yearValue > 2099 ||
+    monthValue < 1 ||
+    monthValue > 12 ||
+    dayValue < 1
+  ) {
+    return false;
+  }
+
+  const lastDayInMonth = new Date(yearValue, monthValue, 0).getDate();
+  return dayValue <= lastDayInMonth;
+}
