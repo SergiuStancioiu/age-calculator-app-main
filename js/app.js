a7a765fcd625044a76bc00d5800cb4d120bf1558
Sergiu Stancioiu <s.sergiu96@gmail.com>
2023-11-08 20:25:42 +0200

    Finished validation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function calculateAge(birthDate) {
   const currentDate = new Date();
   const birthDateObj = new Date(birthDate);
 
-  const years = currentDate.getFullYear() - birthDateObj.getFullYear();
+  let years = currentDate.getFullYear() - birthDateObj.getFullYear();
   const birthMonth = birthDateObj.getMonth();
   const currentMonth = currentDate.getMonth();
   let months = currentMonth - birthMonth;
@@ -41,68 +41,80 @@ function validateDate() {
   const monthError = document.querySelector(".month-error");
   const yearError = document.querySelector(".year-error");
 
+  const dayInputWrap = document.querySelector(".day-input-wrap");
+  const monthInputWrap = document.querySelector(".month-input-wrap");
+  const yearInputWrap = document.querySelector(".year-input-wrap");
+
   const dayValue = dayInput.value.trim();
   const monthValue = monthInput.value.trim();
   const yearValue = yearInput.value.trim();
 
   const currentYear = new Date().getFullYear();
 
-  console.log(currentYear);
-
   if (dayValue === "") {
     dayError.textContent = "This field is required";
+    dayInputWrap.classList.add("border-error");
   } else {
     dayError.textContent = "";
+    dayInputWrap.classList.remove("border-error");
 
     if (!isNumber(dayValue) || dayValue < 1 || dayValue > 31) {
       dayError.textContent = "Must be a valid day";
+      dayInputWrap.classList.add("border-error");
     } else {
       dayError.textContent = "";
+      dayInputWrap.classList.remove("border-error");
     }
   }
 
   if (monthValue === "") {
     monthError.textContent = "This field is required";
+    monthInputWrap.classList.add("border-error");
   } else {
     monthError.textContent = "";
+    monthInputWrap.classList.remove("border-error");
 
     if (!isNumber(monthValue) || monthValue < 1 || monthValue > 12) {
       monthError.textContent = "Must be a valid month";
+      monthInputWrap.classList.add("border-error");
     } else {
       monthError.textContent = "";
+      monthInputWrap.classList.remove("border-error");
     }
   }
 
   if (yearValue === "") {
     yearError.textContent = "This field is required";
+    yearInputWrap.classList.add("border-error");
   } else {
     yearError.textContent = "";
+    yearInputWrap.classList.remove("border-error");
 
-    if (!isNumber(yearValue) || yearValue < 1900 || yearValue > currentYear) {
+    if (!isNumber(yearValue) || yearValue < 1900) {
       yearError.textContent = "Must be a valid year";
+      yearInputWrap.classList.add("border-error");
     } else {
       yearError.textContent = "";
+      yearInputWrap.classList.remove("border-error");
+    }
+
+    if (yearValue > currentYear) {
+      yearError.textContent = "Must be in the past";
+      yearInputWrap.classList.add("border-error");
     }
   }
 
-  if (isNaN(dayValue) || isNaN(monthValue) || isNaN(yearValue)) {
-    document.getElementById("result").textContent =
-      "Invalid input. Please enter valid numbers for dayValue, monthValue, and year.";
+  if (isValidDate(yearValue, monthValue, dayValue)) {
+    const birthDate = new Date(`${yearValue}-${monthValue}-${dayValue}`);
+
+    const age = calculateAge(birthDate);
+
+    showYears.textContent = age.years;
+    showMonths.textContent = age.months;
+    showDays.textContent = age.days;
   } else {
-    if (isValidDate(yearValue, monthValue, dayValue)) {
-      document.getElementById("result").textContent = "Valid date!";
-
-      const birthDate = new Date(`${yearValue}-${monthValue}-${dayValue}`);
-
-      const age = calculateAge(birthDate);
-
-      showYears.textContent = age.years;
-      showMonths.textContent = age.months;
-      showDays.textContent = age.days;
-    } else {
-      document.getElementById("result").textContent =
-        "Invalid date. Please enter a valid date.";
-    }
+    dayError.textContent = "Invalid date";
+    dayInputWrap.classList.add("border-error");
   }
 }
 
